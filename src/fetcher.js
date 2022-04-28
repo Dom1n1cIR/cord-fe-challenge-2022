0bb0fe5f44dec4da5631f60cb6e1b8a35ea8b006
Dom1n1cIR <d.robeson0511@gmail.com>
2022-04-28 21:58:29 +0100

    adding .env file to gitignore and updating fetcher file with api key

--- src/fetcher.js
+++ src/fetcher.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+const API_KEY = process.env.REACT_APP_MOVIE_API_KEY
 
 // TODO: All of your API requests should be in this file
 // See the README file for more information about the APIs you would need to use
