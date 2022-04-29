75ea417b60393225ea934e3da4426df8450288dd
Dom1n1cIR <d.robeson0511@gmail.com>
2022-04-29 14:58:51 +0100

    converting Discover class component into a functional one

--- src/pages/discover/index.js
+++ src/pages/discover/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import styled from 'styled-components';
 
 import * as colors from "../../colors";
@@ -7,50 +7,74 @@ import * as fetcher from "../../fetcher";
 import SearchFilters from "../../components/searchfilter";
 import MovieList from "../../components/movielist";
 
-export default class Discover extends React.Component {
-  constructor (props) {
-    super(props);
+const DiscoverWrapper = styled.main`
+  padding: 35px;
+`
+const MovieResults = styled.div`
+  display: inline-block;
+  width: calc(100% - 295px);
+`
+const MovieFilters = styled.div`
+  width: 280px;
+  float: right;
+  margin-top: 15px;
+`
+const MobilePageTitle = styled.h1`
+  display: none;
+`
+const TotalCount = styled.strong`
+  display: block;
+`
 
-    this.state = {
-      keyword: '',
-      year: 0,
-      results: [],
-      totalCount: 0,
-      genreOptions: [],
-      ratingOptions: [
-        { id: 7.5, name: 7.5 },
-        { id: 8, name: 8 },
-        { id: 8.5, name: 8.5 },
-        { id: 9, name: 9 },
-        { id: 9.5, name: 9.5 },
-        { id: 10, name: 10 }
-      ],
-      languageOptions: [
-        { id: 'GR', name: 'Greek' },
-        { id: 'EN', name: 'English' },
-        { id: 'RU', name: 'Russian' },
-        { id: 'PO', name: 'Polish' }
-      ]
-    };
-  }
+function Discover() {
+  // Our state
+  const [keyword, setKeyword] = useState("");
+  const [year, setYear] = useState(0);
+  const [results, setResults] = useState([]);
+  const [movieDetails, setMovieDetails] = useState(null);
+  const [totalCount, setTotalCount] = useState(0);
+  const [genreOptions, setGenreOptions] = useState([]);
+  const [ratingOptions, setRatingOptions] = useState([
+    { id: 7.5, name: 7.5 },
+    { id: 8, name: 8 },
+    { id: 8.5, name: 8.5 },
+    { id: 9, name: 9 },
+    { id: 9.5, name: 9.5 },
+    { id: 10, name: 10 },
+  ]);
+  const [languageOptions, setLanguageOptions] = useState([
+    { id: "GR", name: "Greek" },
+    { id: "EN", name: "English" },
+    { id: "RU", name: "Russian" },
+    { id: "PO", name: "Polish" },
+  ]);
 
   // TODO: Preload and set the popular movies and movie genres when page loads
-
+  useEffect(() => {
+    fetcher.getPopularMovies(setResults, setTotalCount)
+    fetcher.getMovieGenres(setGenreOptions);
+  }, [])
+  
   // TODO: Update search results based on the keyword and year inputs
+  const searchMovies = async (keyword, year) => {
+    if (keyword === "") {
+      fetcher.getPopularMovies(setResults, setTotalCount);
+    } else {
+      fetcher.getSearchMovie(keyword, setResults, setTotalCount);
+    }
+  };
 
-  render () {
-    const { genreOptions, languageOptions, ratingOptions, totalCount, results } = this.state;
-
-    return (
+  return (
+    <>
       <DiscoverWrapper>
-        <MobilePageTitle>Discover</MobilePageTitle> {/* MobilePageTitle should become visible on mobile devices via CSS media queries*/}
+        <MobilePageTitle>Discovexr</MobilePageTitle> {/* MobilePageTitle should become visible on mobile devices via CSS media queries*/}
         <TotalCount>{totalCount} results</TotalCount>
         <MovieFilters>
           <SearchFilters 
             genres={genreOptions} 
             ratings={ratingOptions}  
             languages={languageOptions}
-            searchMovies={(keyword, year) => this.searchMovies(keyword, year)}
+            searchMovies={(keyword, year) => searchMovies(keyword, year)}
           />
         </MovieFilters>
         <MovieResults>
@@ -60,29 +84,9 @@ export default class Discover extends React.Component {
           />
         </MovieResults>
       </DiscoverWrapper>
-    )
-  }
+    </>
+  )
+
 }
 
-const DiscoverWrapper = styled.main`
-  padding: 35px;
-`
-
-const MovieResults = styled.div`
-  display: inline-block;
-  width: calc(100% - 295px);
-`
-
-const MovieFilters = styled.div`
-  width: 280px;
-  float: right;
-  margin-top: 15px;
-`
-
-const MobilePageTitle = styled.h1`
-  display: none;
-`
-
-const TotalCount = styled.strong`
-  display: block;
-`
+export default Discover
