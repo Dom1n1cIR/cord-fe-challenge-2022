ee31a9faae074639bc55d02f043090f88be691cc
Dom1n1cIR <d.robeson0511@gmail.com>
2022-04-29 13:27:11 +0100

    adding movie endpoints to fetcher file

--- src/fetcher.js
+++ src/fetcher.js
@@ -3,3 +3,15 @@ const API_KEY = process.env.REACT_APP_MOVIE_API_KEY
 
 // TODO: All of your API requests should be in this file
 // See the README file for more information about the APIs you would need to use
+
+export const getPopularMovies = async (setPopularMovies, setTotalCount) => {
+    await axios
+          .get(`https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`)
+          .then((response) => {
+              setPopularMovies(response.data.results);
+              setTotalCount(response.data.total_results);
+          })
+          .catch((error) => {
+              console.log(error);
+          })
+}
